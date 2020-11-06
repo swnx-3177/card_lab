133b46721b40a8d5f3789b4648c57dd52812194e
swnx-3177 <73655917+swnx-3177@users.noreply.github.com>
2020-11-06 17:47:08 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Card</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="card-deck">
+            <div class="card">
+              <img src="pict_1.jpg" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">HLLD is Halloween Day!!</h5>
+                <p class="card-text">Booooo Booooo</p>
+              </div>
+              <div class="card-footer">
+                <img style="border-radius: 30%;" src="pict_avt3.png" height=30>
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </div>
+            </div>
+            <div class="card">
+              <img src="pict_2.jpg" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">Beauty and The Beast</h5>
+                <p class="card-text">Heart Heart</p>
+              </div>
+              <div class="card-footer">
+                <img style="border-radius: 30%;" src="pict_avt2.png" height=30>
+                <small class="text-muted">Last updated 3 hours ago</small>
+              </div>
+            </div>
+            <div class="card">
+              <img src="pict_3.jpg" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">.......</h5>
+                <p class="card-text">Yes it's a empty</p>
+              </div>
+              <div class="card-footer">
+                <img style="border-radius: 30%;" src="pict_avt1.png" height=30>
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </div>
+            </div>
+          </div>
+    </div>
+    
+</body>
+</html>
